ee4e58305fcb964a85980eb3ea9e3a3e3147afc9
PTxDE*SS <desspertsouji@yahoo.co.jp>
2015-11-19 09:34:55 +0900

    エネミーの初期化を遅らせる処理の追加

--- Scene/Gamemain/Gamemain.cpp
+++ Scene/Gamemain/Gamemain.cpp
@@ -2,35 +2,39 @@
 
 Gamemain::Gamemain(){
 
-    Setup();
-    
+
 
 }
 
 
 void Gamemain::Setup(){
 	map.Setup();
-    player.SetPos(map.GetPlayerPos());
-    enemylist.SetPos(map.GetEnemyPos(),1);
-    
+	count = 0;
+	enemy_number = 0;
 }
 
 void Gamemain::Update(){
-
+	int index;
+	count++;
+	index = (count) % 8;
+	if (index == 1){
+		enemy_number++;
+		if (enemy_number <= 5){
+			enemylist.PushEnemy(map.GetEnemyPos(),enemy_number);
+		}
+	}
 	map.Update();
     player.SetSelectItem(ui.Select(player.GetSelectItem()));
     map.Editor(player.GetSelectItem(), player.MouseClick());
-    player.Update();
+	
 }
 
 void Gamemain::Draw(){
-
     ui.Draw();
     ui.Cost(player.GetCost());
     ui.SelectDraw(player.GetSelectItem());
 	map.Draw();
-    player.Draw();
-    enemylist.Draw();
+	enemylist.Draw();
 }
 
 
--- Scene/Gamemain/Gamemain.h
+++ Scene/Gamemain/Gamemain.h
@@ -5,15 +5,17 @@
 #include "Ui/Ui.h"
 #include "Map/Map.h"
 #include "Player/Player.h"
-#include "Enemylist/Enemylist.h"
+#include "Enemylist\Enemylist.h"
+
 
 class Gamemain : public Classbase{
 private:
     Ui ui;
     Player player;
 	Map map;
-    Enemylist enemylist;
-
+	Enemylist enemylist;
+	int count;
+	int enemy_number;
 public:
 	Gamemain();
 
